420e76a2221614ae5910248bdbfe3b490da86e6e
Jamie Bowman <mrjamiebowman@gmail.com>
2020-06-21 09:40:14 -0400

    updated run script.

--- run.ps1
+++ run.ps1
@@ -1,15 +1,11 @@
 # clear screen
-docker rm -f $(docker ps -aq)
+#docker rm -f $(docker ps -aq)
 cls
 
 docker login
 
 docker run -d --name jenkinsci -p 8080:8080 mrjamiebowman/jenkins-windows
-
 #docker run --rm -it --name jenkinsci -p 8080:8080 mrjamiebowman/jenkins-windows powershell
-
 #docker run -it jenkinsci type C:\Users\ContainerAdministrator\.jenkins\secrets\initialAdminPassword
 
-#start https://localhost:8080
-
-docker ps -a
+start https://localhost:8080
