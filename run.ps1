4fc11c6efd1e4d54e8f6040d362652c530c11947
Jamie Bowman <mrjamiebowman@gmail.com>
2020-06-21 09:46:47 -0400

    jenkins does't have ssl setup.

--- run.ps1
+++ run.ps1
@@ -8,4 +8,4 @@ docker run -d --name jenkinsci -p 8080:8080 mrjamiebowman/jenkins-windows
 #docker run --rm -it --name jenkinsci -p 8080:8080 mrjamiebowman/jenkins-windows powershell
 #docker run -it jenkinsci type C:\Users\ContainerAdministrator\.jenkins\secrets\initialAdminPassword
 
-start https://localhost:8080
+start http://localhost:8080
